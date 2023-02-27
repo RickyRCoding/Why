4807bb8101e31ab52823630a691875691b901d71
RickyRCoding <ricardorizk2@gmail.com>
2023-02-27 15:21:53 -0500

    Create text variable in TextChat.

--- lib/widgets/text_chat.dart
+++ lib/widgets/text_chat.dart
@@ -1,10 +1,25 @@
-// ignore_for_file: use_key_in_widget_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors_in_immutables
 
 import "package:flutter/material.dart";
 
 class TextChat extends StatelessWidget {
+  final String text;
+
+  TextChat(
+    this.text,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Card(
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Text("Hello there!"),
+          ),
+        ),
+      ],
+    );
   }
 }
