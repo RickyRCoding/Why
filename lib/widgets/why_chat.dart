e85106615c797e5657132cefe464ecfc537f4cfa
RickyRCoding <ricardorizk2@gmail.com>
2023-03-01 17:31:29 -0500

    Copy text_chat as why_chat.dart

---
+++ lib/widgets/why_chat.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors_in_immutables, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import "package:flutter/material.dart";
+
+class TextChat extends StatelessWidget {
+  final String text;
+
+  TextChat(
+    this.text,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Card(
+            elevation: 5,
+            color: Colors.purple[50],
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(text),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
